2a7b1db1f618b6c918a5601876a2803c2b190f2e
Living58 <97684888+Living58@users.noreply.github.com>
2022-03-24 10:49:34 +0100

    Add files via upload

---
+++ Oblig8/src/App.js
@@ -0,0 +1,25 @@
+// Samarbeid med Yvonne Nielsen, Anna Marita Meum og Fredrik Kapoun Olsen
+
+import {Route, Routes} from 'react-router-dom'
+import Actor from './components/Actor'
+import ActorName from './components/ActorName'
+import Movies from './components/Movies'
+import Navigation from './components/Navigation'
+
+
+export default function App() {
+  // Write JavaScript, use Hooks, add state and more
+
+  return (
+    <>
+    <main className="m-auto h-screen w-full bg-cyan-400">
+      <Navigation />
+      <Routes>
+        <Route path="Movies" element={<Movies />}/>
+        <Route path="Actor" element={<Actor />}/>
+        <Route path="Actor/:slug" element={<ActorName />}/>
+      </Routes>
+    </main>
+    </>
+  )
+}
